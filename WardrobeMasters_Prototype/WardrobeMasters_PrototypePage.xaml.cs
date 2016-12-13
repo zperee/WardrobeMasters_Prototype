3147876cc63afaf7b3b10d803fcdd9890333b65c
zperee <elia.perenzin@bluewin.ch>
2016-12-13 09:36:19 +0100

    init commit

---
+++ WardrobeMasters_Prototype/WardrobeMasters_PrototypePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace WardrobeMasters_Prototype
+{
+	public partial class WardrobeMasters_PrototypePage : ContentPage
+	{
+		public WardrobeMasters_PrototypePage()
+		{
+			InitializeComponent();
+		}
+	}
+}
